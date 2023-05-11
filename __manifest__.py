fb6b892dafbab1eb18a6c0cbe6c812f92f4f58e7
Andres Cusirramos Marquez Mares <andres.cusirramos@ucsp.edu.pe>
2023-05-11 10:58:19 -0500

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Real State",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Inka Labs",
+    'website': "https://inka-labs.com/",
+    'category': 'Sales/state',
+    # data files always loaded at installation
+    'data': [
+     'security/ir.model.access.csv',
+     'views/estate_property_views.xml',
+     'views/estate_menus.xml',
+    ],
+}
---
+++ models/crm_recurring_plan.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+class RecurringPlan(models.Model):
+    # Names
+    _name = "estate.property"
+    _description = "Estate Property"
+
+
+    # Functions
+
+
+    def _default_date_availability(self):
+        return fields.Date.context_today(self) + relativedelta(months=3)
+
+
+    name = fields.Char(required = True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Datetime(copy = False, default=lambda self: self._default_date_availability() )
+    expected_price = fields.Float(Required = True)
+    selling_price = fields.Float("Selling Price", readonly = True, copy = False)
+    bedrooms = fields.Integer(default = 2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Garden_Orientation',
+        selection=[('north', 'North'), ('south', 'South'),('east', 'East'),('west', 'West')])
+    active = fields.Boolean()
+    status = fields.Selection(
+        string='Status',
+        selection=[('Offer Recieve', 'Offer Recieve'),('Offer Accepted', 'Offer Accepted'), ('Offer Rejected', 'Offer Rejected')])
+
+
